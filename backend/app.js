621274ae9e0cafabb5a244e41a6474082b3343c4
Anastasia Belova <nastasya.belowa@yandex.ru>
2023-08-05 14:18:30 +0300

    порт 3000

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const route = require('./routes/route');
 const centralError = require('./middlewares/centralError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(express.json());
